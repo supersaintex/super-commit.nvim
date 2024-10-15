1fb6454455e1c6080f52b2b93824eac96b8f70e0
KKuze <kaikuze1107@gmail.com>
2024-10-16 02:50:27 +0900

    feat: show the diff of the selected file

--- lua/super-commit/super-commit.lua
+++ lua/super-commit/super-commit.lua
@@ -3,6 +3,13 @@ local git_cmds = require('super-commit/git/commands')
 
 local M = {}
 
+function M.show_selected_diff()
+    git_cmds.show_diff_cmd_output(
+        window_open.bufnum_table[2],
+        vim.api.nvim_get_current_line()
+    )
+end
+
 function M.set_autocmd()
   M.augroup = vim.api.nvim_create_augroup('SuperCommitWindow', {})
   vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
@@ -10,11 +17,18 @@ function M.set_autocmd()
     pattern = '*.super',
     -- pattern = 'COMMIT_EDITMSG',
     callback = function()
-      local bufnum_table = window_open.setup()
+      window_open.setup()
+      local bufnum_table = window_open.bufnum_table
       local commands = git_cmds.commands
       for k, cmd in pairs(commands) do
           git_cmds.show_command_output(bufnum_table[k], cmd)
       end
+      vim.api.nvim_set_keymap(
+          'n',
+          '<CR>',
+          [[<Cmd>lua require('super-commit/super-commit').show_selected_diff()<CR>]],
+          { noremap = true, silent = true }
+	)
     end,
   })
 end
