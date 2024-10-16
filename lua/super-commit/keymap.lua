706684b265fcea4b854bbed68ee7008f4b9b548e
KKuze <kaikuze1107@gmail.com>
2024-10-17 01:12:53 +0900

    fixup! feat: get the cursor line when the Enter key is pressed

--- lua/super-commit/keymap.lua
+++ lua/super-commit/keymap.lua
@@ -1,13 +1,12 @@
 local window_open = require('super-commit/window/open')
 local git_cmds = require('super-commit/git/commands')
-local filepath = require('super-commit/filepath/get')
 
 local M = {}
 
 function M.enter_in_normalmode()
     git_cmds.show_diff_cmd_output(
         window_open.bufnums[2],
-        filepath.get()
+        vim.api.nvim_get_current_line()
     )
 end
 
