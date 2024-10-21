abc27bfba351aefc5556cdbc1516f35378d82153
manhatsu <manhatsu@gmail.com>
2024-10-22 00:28:18 +0900

    chore: simplify function/variable name and follow pre-commit character limit

--- lua/super-commit/super-commit.lua
+++ lua/super-commit/super-commit.lua
@@ -4,7 +4,7 @@ local git_cmds = require('super-commit/git/commands')
 local M = {}
 
 function M.show_selected_diff()
-    git_cmds.show_diff_cmd_output(
+    git_cmds.show_diff(
         window_open.bufnum_table[2],
         vim.api.nvim_get_current_line()
     )
@@ -19,14 +19,15 @@ function M.set_autocmd()
     callback = function()
       window_open.setup()
       local bufnum_table = window_open.bufnum_table
-      local commands = git_cmds.commands
-      for k, cmd in pairs(commands) do
-          git_cmds.show_command_output(bufnum_table[k], cmd)
+      local command_table = git_cmds.command_table
+      for k, cmd in pairs(command_table) do
+          git_cmds.show_output(bufnum_table[k], cmd)
       end
       vim.api.nvim_set_keymap(
           'n',
           '<CR>',
-          [[<Cmd>lua require('super-commit/super-commit').show_selected_diff()<CR>]],
+          '<Cmd>lua require("super-commit/super-commit")' ..
+          '.show_selected_diff()<CR>',
           { noremap = true, silent = true }
 	)
     end,
