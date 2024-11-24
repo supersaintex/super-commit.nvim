90843ff2bae0f70675e57e9d0f35b1fb7d94eab4
seiay <seiieshima831@gmail.com>
2024-11-24 21:03:01 +0900

    feat(buffer.lua): set buffer name

--- lua/super-commit/buffer.lua
+++ lua/super-commit/buffer.lua
@@ -5,6 +5,12 @@ function M.getid(git_key)
   return bufids[git_key]
 end
 
+local name = {}
+name["commit"] = "commit message"
+name["status"] = "git status"
+name["filelist"] = "staged files"
+name["diff"] = "diff"
+
 local filetype = {}
 filetype["commit"] = "gitcommit"
 filetype["status"] = "gitrebase"
@@ -12,6 +18,9 @@ filetype["diff"] = "git"
 
 function M.setup(git_key)
   bufids[git_key] = vim.api.nvim_create_buf(false, true)
+  if (name[git_key]) then
+    vim.api.nvim_buf_set_name(bufids[git_key], name[git_key])
+  end
   if (filetype[git_key]) then
     vim.api.nvim_set_option_value("filetype", filetype[git_key], {buf = bufids[git_key]})
   end
