1c779f981a3f9681664472bd8ea1f47a68aa5141
seiay <seiieshima831@gmail.com>
2024-11-24 20:43:12 +0900

    feat(buffer.lua): add syntax highlighting

--- lua/super-commit/buffer.lua
+++ lua/super-commit/buffer.lua
@@ -5,8 +5,16 @@ function M.getid(git_key)
   return bufids[git_key]
 end
 
+local filetype = {}
+filetype["commit"] = "gitcommit"
+filetype["status"] = "gitrebase"
+filetype["diff"] = "git"
+
 function M.setup(git_key)
   bufids[git_key] = vim.api.nvim_create_buf(false, true)
+  if (filetype[git_key]) then
+    vim.api.nvim_set_option_value("filetype", filetype[git_key], {buf = bufids[git_key]})
+  end
 end
 
 return M
